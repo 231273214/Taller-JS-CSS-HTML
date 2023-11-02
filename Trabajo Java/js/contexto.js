af51484d7a621ea4cbd4abb1c79ab18239e57ce8
Alekxander <142550167+Alekxander00@users.noreply.github.com>
2023-11-02 16:18:20 -0500

    Delete Trabajo Java directory

--- Trabajo Java/js/contexto.js
+++
@@ -1,8 +0,0 @@
-function searchAndHighlight() {
-    const searchText = document.getElementById("search-input").value;
-    const userText = document.getElementById("user-text").value;
-    const highlightedText = document.getElementById("highlighted-text");
-    const regex = new RegExp(searchText, "g");
-    const resultText = userText.replace(regex, '<span class="highlight">$&</span>');
-    highlightedText.innerHTML = resultText;
-}
