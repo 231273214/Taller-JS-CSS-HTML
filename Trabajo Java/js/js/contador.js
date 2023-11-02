4e40dbbf10a18258c3add989f1d409dbf21fa6f4
Alekxander <142550167+Alekxander00@users.noreply.github.com>
2023-11-02 15:45:06 -0500

    Delete Trabajo Java/js/js directory

--- Trabajo Java/js/js/contador.js
+++
@@ -1,8 +0,0 @@
-function contarCaracteres() {
-    const textoInput = document.getElementById('texto');
-    const contadorSpan = document.getElementById('contador');
-
-    const texto = textoInput.value;
-    const caracteres = texto.length;
-    contadorSpan.textContent = caracteres;
-}
